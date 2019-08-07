c58385633911d33e4843abc0d2f313a4868b7430
gaoguangxiao <1137747423@qq.com>
2019-08-07 21:54:37 +0800

    提是文件

---
+++ RPHeadPortraitBubble/e.h
@@ -0,0 +1,17 @@
+//
+//  e.h
+//  RPHeadPortraitBubble
+//
+//  Created by gaoguangxiao on 2019/8/7.
+//  Copyright © 2019 Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface e : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RPHeadPortraitBubble/e.m
@@ -0,0 +1,21 @@
+//
+//  e.m
+//  RPHeadPortraitBubble
+//
+//  Created by gaoguangxiao on 2019/8/7.
+//  Copyright © 2019 Tao. All rights reserved.
+//
+
+#import "e.h"
+
+@implementation e
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
